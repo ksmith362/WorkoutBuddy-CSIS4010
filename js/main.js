9debecc50dc74d097b8c4bd54ab3bcd77400f507
Kate Smith <J02251542@jwu.edu>
2022-10-11 17:21:03 -0400

    Complete redesign of index.html, others coming soon

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+const header = document.querySelector('header');
+const headerMenu = header.querySelector('.menu');
+const btnMenu = header.querySelector('button.show-for-small-only');
+
+// toggle menu on/off
+btnMenu.addEventListener('click',function(){
+  // show menu by removing 'show-for-medium' class
+  headerMenu.classList.toggle('show-for-medium');
+  // add toggle state indicator
+  this.classList.toggle('btnToggle');
+  // change button text for toggled state
+  changeBtnText();
+})
+
+// change button text for toggled state
+function changeBtnText(){
+  if(btnMenu.classList.contains('btnToggle')){
+    btnMenu.textContent = 'X';
+  } else {
+    btnMenu.textContent = 'Menu'
+  }
+}
